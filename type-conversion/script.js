f548f9199dd401bacd64622d0a5fad1b7ddbf1eb
Steady-K <tkdlxm56@gmail.com>
2025-02-27 21:23:46 +0900

    lean JavaScript-Type-Conversion

---
+++ type-conversion/script.js
@@ -0,0 +1,42 @@
+// // 1. Javascript 함수를 사용하여여
+
+// let val;
+
+// val = String(111);
+// val = String(8 + 4);
+
+// // Boolean to String
+// val = String(false);
+
+// // Date to String
+// val = String(new Date());
+
+// // Array to String
+// val = String([1, 2, 3, 4, 5]);
+
+// // toString()
+// val = (5).toString();
+
+// // String to number
+// val = Number('1');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number([1, 2, 3]);
+// // NaN = Not a Number
+
+// val = parseInt('111.40');
+// val = parseFloat('111.40');
+
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+
+// // Javascript 자체에 의해 자동으로
+
+const val1 = String(2);
+const val2 = 3;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
