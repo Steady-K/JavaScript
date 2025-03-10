d8d9060cbe5f3f4d5ee904da92f9337e4bf18668
Steady-K <tkdlxm56@gmail.com>
2025-03-10 18:13:51 +0900

    JavaScript
    Symbol

---
+++ symbol/script.js
@@ -0,0 +1,42 @@
+// // const sym1 = Symbol();
+// // const sym2 = Symbol();
+// // const sym3 = Symbol('hi');
+
+// // console.log(sym1.description);
+// // console.log(sym2);
+// // console.log(sym1 === sym2);
+
+// // console.log(sym3.description);
+
+// let carA = {
+//   id: 1,
+//   name: 'moring',
+//   brand: 'kia',
+//   price: 1000,
+// };
+
+// const idSym = Symbol('id');
+// carA[idSym] = 300;
+
+// // console.log(carA);
+
+// console.log(Object.getOwnPropertyNames(carA));
+
+// for (const key in carA) {
+//   console.log(key);
+// }
+
+// console.log(Object.getOwnPropertySymbols(carA));
+
+// symbol의 description이 같더라도 false
+// const sym1 = Symbol('sym1');
+// const sym2 = Symbol('sym1');
+
+// for을 이용하여 심볼을 생성하면 전역 심볼이 되어서 Symbol 값이 같음음
+const sym1 = Symbol.for('sym1');
+const sym2 = Symbol.for('sym1222');
+
+// console.log(sym1 === sym2);
+
+console.log(Symbol.keyFor(sym1));
+console.log(Symbol.keyFor(sym2));
